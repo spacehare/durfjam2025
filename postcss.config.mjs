2afc190396302b52fe7428d0d7c2d6ef2483aa2e
spacehare <dantath425@gmail.com>
2025-10-18 11:16:30 -0400

    fuck around with formatting
    some stuff isn't working at all. like `gap` for example

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,7 +4,6 @@ export default {
 		"@tailwindcss/postcss": {},
 		"postcss-preset-env": {
 			// https://github.com/Kozea/WeasyPrint/issues/1998
-			stage: 1,
 			features: {
 				// https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
 				// https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-custom-properties#readme
